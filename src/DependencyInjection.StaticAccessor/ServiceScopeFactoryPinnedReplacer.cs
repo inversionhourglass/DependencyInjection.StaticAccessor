5a58b77c47ff47662d9a9ba8b5757bed0aa1e8c5
ihourglass <inversionhourglass@outlook.com>
2024-09-13 16:31:14 +0800

    Catch any exception thrown by ServiceScopeFactoryPinnedReplacer and rethrow a NotSupportedException.

--- src/DependencyInjection.StaticAccessor/ServiceScopeFactoryPinnedReplacer.cs
+++ src/DependencyInjection.StaticAccessor/ServiceScopeFactoryPinnedReplacer.cs
@@ -15,34 +15,41 @@ namespace DependencyInjection.StaticAccessor
         /// </summary>
         public void Handle(IServiceProvider serviceProvider)
         {
-            var tProvider = serviceProvider.GetType();
-            var pCallSiteFactory = tProvider.GetProperty("CallSiteFactory", BindingFlags.NonPublic | BindingFlags.Instance);
-            var callSiteFactory = pCallSiteFactory.GetValue(serviceProvider);
-            var fCallSiteCache = callSiteFactory.GetType().GetField("_callSiteCache", BindingFlags.NonPublic | BindingFlags.Instance);
-            var callSiteCache = (IDictionary)fCallSiteCache.GetValue(callSiteFactory);
-            object? key = null;
-            foreach (DictionaryEntry item in callSiteCache)
+            try
             {
-                var pServiceIdentifier = item.Key.GetType().GetProperty("ServiceIdentifier");
-                var serviceIdentifier = pServiceIdentifier.GetValue(item.Key);
-                var pServiceType = serviceIdentifier.GetType().GetProperty("ServiceType");
-                var serviceType = (Type)pServiceType.GetValue(serviceIdentifier);
-                if (serviceType == typeof(IServiceScopeFactory))
+                var tProvider = serviceProvider.GetType();
+                var pCallSiteFactory = tProvider.GetProperty("CallSiteFactory", BindingFlags.NonPublic | BindingFlags.Instance);
+                var callSiteFactory = pCallSiteFactory.GetValue(serviceProvider);
+                var fCallSiteCache = callSiteFactory.GetType().GetField("_callSiteCache", BindingFlags.NonPublic | BindingFlags.Instance);
+                var callSiteCache = (IDictionary)fCallSiteCache.GetValue(callSiteFactory);
+                object? key = null;
+                foreach (DictionaryEntry item in callSiteCache)
                 {
-                    key = item.Key;
-                    break;
+                    var pServiceIdentifier = item.Key.GetType().GetProperty("ServiceIdentifier");
+                    var serviceIdentifier = pServiceIdentifier.GetValue(item.Key);
+                    var pServiceType = serviceIdentifier.GetType().GetProperty("ServiceType");
+                    var serviceType = (Type)pServiceType.GetValue(serviceIdentifier);
+                    if (serviceType == typeof(IServiceScopeFactory))
+                    {
+                        key = item.Key;
+                        break;
+                    }
                 }
+
+                var pRoot = tProvider.GetProperty("Root", BindingFlags.NonPublic | BindingFlags.Instance);
+                var root = (IServiceScopeFactory)pRoot.GetValue(serviceProvider);
+                var tCallSite = tProvider.Assembly.GetType("Microsoft.Extensions.DependencyInjection.ServiceLookup.ConstantCallSite");
+                var ctor = tCallSite.GetConstructor([typeof(Type), typeof(object)]);
+                var factory = ctor.Invoke([typeof(IServiceScopeFactory), new PinnedServiceScopeFactory(root)]);
+
+                callSiteCache[key] = factory;
+
+                SetRootServices(serviceProvider);
             }
-
-            var pRoot = tProvider.GetProperty("Root", BindingFlags.NonPublic | BindingFlags.Instance);
-            var root = (IServiceScopeFactory)pRoot.GetValue(serviceProvider);
-            var tCallSite = tProvider.Assembly.GetType("Microsoft.Extensions.DependencyInjection.ServiceLookup.ConstantCallSite");
-            var ctor = tCallSite.GetConstructor([typeof(Type), typeof(object)]);
-            var factory = ctor.Invoke([typeof(IServiceScopeFactory), new PinnedServiceScopeFactory(root)]);
-
-            callSiteCache[key] = factory;
-
-            SetRootServices(serviceProvider);
+            catch
+            {
+                throw new NotSupportedException("The ServiceProvider implementation has changed; please submit an issue to https://github.com/inversionhourglass/DependencyInjection.StaticAccessor/issues.");
+            }
         }
 
         private void SetRootServices(IServiceProvider serviceProvider)
