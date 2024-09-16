6ca51460808082f7220db8efe5818b2bb4a584d4
ihourglass <inversionhourglass@outlook.com>
2024-09-16 09:05:29 +0800

    Rename the interface name from IScopeProvider to IScopeForward. Add a new abstract class FoolScopeProvider.

--- src/DependencyInjection.StaticAccessor/IScopeProvider.cs
+++ src/DependencyInjection.StaticAccessor/IScopeProvider.cs
@@ -1,11 +1,9 @@
 ﻿using Microsoft.Extensions.DependencyInjection;
-using System;
 
 namespace DependencyInjection.StaticAccessor
 {
     /// <summary>
-    /// Preferentially obtain the <see cref="IServiceScope"/> from the <see cref="IServiceProvider"/>s,
-    /// return the default <see cref="IServiceScope"/> if all <see cref="IScopeProvider.Scope"/> instances return null.
+    /// <see cref="IServiceScope"/> provider
     /// </summary>
     public interface IScopeProvider
     {
