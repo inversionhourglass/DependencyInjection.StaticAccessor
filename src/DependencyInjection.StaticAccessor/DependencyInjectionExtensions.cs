61517c95780b7c0055eb401dd83a12657dbd4d2e
ihourglass <inversionhourglass@outlook.com>
2024-09-17 23:51:01 +0800

    Add some useful extension methods.

---
+++ src/DependencyInjection.StaticAccessor/DependencyInjectionExtensions.cs
@@ -0,0 +1,38 @@
+﻿using DependencyInjection.StaticAccessor;
+using System;
+
+namespace Microsoft.Extensions.DependencyInjection
+{
+    /// <summary>
+    /// </summary>
+    public static class DependencyInjectionExtensions
+    {
+        /// <summary>
+        /// </summary>
+        public static IServiceCollection AddScopeForward(this IServiceCollection services, Type forwardType)
+        {
+            return services.AddSingleton(typeof(IScopeForward), forwardType);
+        }
+
+        /// <summary>
+        /// </summary>
+        public static IServiceCollection AddScopeForward<T>(this IServiceCollection services) where T : class, IScopeForward
+        {
+            return services.AddSingleton<IScopeForward, T>();
+        }
+
+        /// <summary>
+        /// </summary>
+        public static IServiceCollection AddScopeGoalie(this IServiceCollection services, Type goalieType)
+        {
+            return services.AddSingleton(typeof(IScopeGoalie), goalieType);
+        }
+
+        /// <summary>
+        /// </summary>
+        public static IServiceCollection AddScopeGoalie<T>(this IServiceCollection services) where T : class, IScopeGoalie
+        {
+            return services.AddSingleton<IScopeGoalie, T>();
+        }
+    }
+}
