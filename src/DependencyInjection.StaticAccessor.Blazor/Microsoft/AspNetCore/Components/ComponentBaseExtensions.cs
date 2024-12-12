55c508f9bb15898d769923f4c95b908782a9d0dd
ihourglass <inversionhourglass@outlook.com>
2024-12-12 18:11:33 +0800

    Use UnsafeAccessorAttribute to access the method HandleEventAsync in ComponentBase.

--- src/DependencyInjection.StaticAccessor.Blazor/Microsoft/AspNetCore/Components/ComponentBaseExtensions.cs
+++ src/DependencyInjection.StaticAccessor.Blazor/Microsoft/AspNetCore/Components/ComponentBaseExtensions.cs
@@ -1,7 +1,7 @@
 ﻿using DependencyInjection.StaticAccessor;
 using DependencyInjection.StaticAccessor.Blazor;
 using System;
-using System.Collections.Concurrent;
+using System.Runtime.CompilerServices;
 using System.Threading.Tasks;
 
 namespace Microsoft.AspNetCore.Components
@@ -10,27 +10,17 @@ namespace Microsoft.AspNetCore.Components
     /// </summary>
     public static class ComponentBaseExtensions
     {
-        private static readonly ConcurrentDictionary<Type, Func<ComponentBase, EventCallbackWorkItem, object?, Task>> _Cache = [];
-
         /// <summary>
         /// Save the <see cref="IServiceProvider"/> before executing the callback.
         /// </summary>
         public static Task PinnedScopeHandleEventAsync<TComponent>(this TComponent component, EventCallbackWorkItem callback, object? arg) where TComponent : ComponentBase, IServiceProviderHolder
         {
-            return component.PinnedScopeHandleEventAsync(component.GetType().BaseType!.BaseType!, callback, arg);
-        }
-
-        /// <summary>
-        /// Save the <see cref="IServiceProvider"/> before executing the callback.
-        /// </summary>
-        public static Task PinnedScopeHandleEventAsync<TComponent>(this TComponent component, Type componentType, EventCallbackWorkItem callback, object? arg) where TComponent : ComponentBase, IServiceProviderHolder
-        {
-            var mHandleEventAsync = componentType.GetMethod("Microsoft.AspNetCore.Components.IHandleEvent.HandleEventAsync", System.Reflection.BindingFlags.NonPublic | System.Reflection.BindingFlags.Instance)!;
             PinnedScope.Scope = new FoolScope(component.ServiceProvider);
 
-            var func = _Cache.GetOrAdd(mHandleEventAsync.DeclaringType!, t => mHandleEventAsync.CreateDelegate<Func<ComponentBase, EventCallbackWorkItem, object?, Task>>());
-
-            return func(component, callback, arg);
+            return HandleEventAsync(component, callback, arg);
         }
+
+        [UnsafeAccessor(UnsafeAccessorKind.Method, Name = "Microsoft.AspNetCore.Components.IHandleEvent.HandleEventAsync")]
+        private extern static Task HandleEventAsync(ComponentBase handleEvent, EventCallbackWorkItem callback, object? arg);
     }
 }
