f6cae605e3a5fb601ca4d162508101de8e639d51
ihourglass <inversionhourglass@outlook.com>
2024-12-13 16:13:22 +0800

    Forgot to remove the test code.

--- src/DependencyInjection.StaticAccessor.Blazor/DependencyInjection/StaticAccessor/Blazor/PinnedScopeOwningComponentBase.cs
+++ src/DependencyInjection.StaticAccessor.Blazor/DependencyInjection/StaticAccessor/Blazor/PinnedScopeOwningComponentBase.cs
@@ -9,7 +9,7 @@ namespace DependencyInjection.StaticAccessor.Blazor
     /// <summary>
     /// Inherit from <see cref="OwningComponentBase"/> and automatically set <see cref="PinnedScope.Scope"/>
     /// </summary>
-    public class PinnedScopeOwningComponentBase : OwningComponentBase, IHandleEvent, IServiceProviderHolder, IDisposable
+    public class PinnedScopeOwningComponentBase : OwningComponentBase, IHandleEvent, IServiceProviderHolder
     {
 #pragma warning disable CS8618 // Non-nullable field must contain a non-null value when exiting constructor. Consider declaring as nullable.
         private IServiceProvider _serviceProvider;
@@ -41,11 +41,6 @@ namespace DependencyInjection.StaticAccessor.Blazor
         Task IHandleEvent.HandleEventAsync(EventCallbackWorkItem callback, object? arg)
         {
             return this.PinnedScopeHandleEventAsync(callback, arg);
-        }
-
-        void IDisposable.Dispose()
-        {
-
         }
     }
 }
---
+++ test/TestAssemblies/BlazorServerApp/Utils.cs
@@ -0,0 +1,12 @@
+﻿using DependencyInjection.StaticAccessor;
+
+namespace BlazorServerApp
+{
+    public static class Utils
+    {
+        public static void M()
+        {
+            Console.WriteLine($"Utils.M: {PinnedScope.ScopedServices?.GetHashCode()}");
+        }
+    }
+}
